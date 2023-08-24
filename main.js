a1dbfb41586c1235bb8fa35a0d24fca9cd8bc50f
HesaM <hesam.titan.666@gmail.com>
2023-08-24 16:55:15 +0330

    IIFE

--- main.js
+++ main.js
@@ -86,24 +86,29 @@
 //   bar('hesam')
  //----------------------------------------------------
  //call/apply/bind
- const user = {
-  firstName: "hesam",
-  lastName: "behnami",
-  getFullName: function (country) {
-    return `${this.firstName} ${this.lastName} ${country} `;
-  },
-};
+//  const user = {
+//   firstName: "hesam",
+//   lastName: "behnami",
+//   getFullName: function (country) {
+//     return `${this.firstName} ${this.lastName} ${country} `;
+//   },
+// };
 
 
-const secondUser = {
-  firstName: "ali",
-  lastName: "Behnia",
-};
+// const secondUser = {
+//   firstName: "ali",
+//   lastName: "Behnia",
+// };
 
-//call
-// console.log(user.getFullName.call(secondUser,"iran"));
-// apply
-// console.log(user.getFullName.apply(secondUser,["tehran"]));
-//bind
-const bind = user.getFullName.bind(secondUser,'iran')
-console.log(bind())
+// //call
+// // console.log(user.getFullName.call(secondUser,"iran"));
+// // apply
+// // console.log(user.getFullName.apply(secondUser,["tehran"]));
+// //bind
+// const bind = user.getFullName.bind(secondUser,'iran')
+// console.log(bind())
+ //----------------------------------------------------
+ //IIFE
+ (function () {
+  console.log('hello worrld')
+})()
