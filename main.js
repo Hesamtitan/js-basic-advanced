5ad69c8a20f5d8f8960a83c4c5a9266569ea92df
HesaM <hesam.titan.666@gmail.com>
2023-08-24 16:47:20 +0330

    call apply bind

--- main.js
+++ main.js
@@ -64,13 +64,46 @@
 //----------------------------------------------------
 // callBacks
 
-const callBack = () => {
-    console.log("call back");
-  };
+// const callBack = () => {
+//     console.log("call back");
+//   };
   
-  const myFunction = (cb) => {
-    cb();
-  };
+//   const myFunction = (cb) => {
+//     cb();
+//   };
   
-  myFunction(callBack);
+//   myFunction(callBack);
+  //----------------------------------------------------
+  //Returning  functions
+
+//   const foo = () => {
+//     return (name) => {
+//       console.log(`wellcome ${name}`);
+//     };
+//   };
+//   const bar = foo();
   
+//   bar('hesam')
+ //----------------------------------------------------
+ //call/apply/bind
+ const user = {
+  firstName: "hesam",
+  lastName: "behnami",
+  getFullName: function (country) {
+    return `${this.firstName} ${this.lastName} ${country} `;
+  },
+};
+
+
+const secondUser = {
+  firstName: "ali",
+  lastName: "Behnia",
+};
+
+//call
+// console.log(user.getFullName.call(secondUser,"iran"));
+// apply
+// console.log(user.getFullName.apply(secondUser,["tehran"]));
+//bind
+const bind = user.getFullName.bind(secondUser,'iran')
+console.log(bind())
